1e64de20a01312ec6d5b9501f26d6cc38ce32a58
pranavdeshmukh099 <pranavdeshmukh099@gmail.com>
2025-09-03 13:01:15 +0530

    Your message

--- Frontend/doctor/src/api.js
+++ Frontend/doctor/src/api.js
@@ -1,5 +1,6 @@
 
-export const API = process.env.REACT_APP_API || 'http://localhost:18012';
+// export const API = process.env.REACT_APP_API || 'http://localhost:18012';
+export const API = process.env.REACT_APP_API || 'https://opd-system.onrender.com';
 export function authHeader(){
 const token = localStorage.getItem("token");
   if (!token) return {};
